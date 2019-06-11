3447ccedd0be9556669f5a76364b5d3aaa68c0cc
Aditya Sharma <sharmaaditya570191@gmail.com>
2019-06-12 04:23:51 +0530

    Installer Script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+gnome-terminal
+git clone https://github.com/sharmaaditya570191/Dark-Theme.git
+cp Dark-Theme/userContent.css .mozilla/firefox/*.default/chrome/userContent.css
+echo "Setup complete!"
+echo "Restart Firefox after closing all tabs"
