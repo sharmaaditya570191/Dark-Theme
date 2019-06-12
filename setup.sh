d20d5c396ae7496c04b9db7d96461e1134312030
Aditya Sharma <sharmaaditya570191@gmail.com>
2019-06-12 06:04:06 +0530

    Update script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 cd
+cd .mozilla/firefox/*.default
+mkdir chrome
+cd chrome
+touch userContent.css
+cd
 cp Dark-Theme/userContent.css .mozilla/firefox/*.default/chrome/userContent.css
 echo "Setup complete!"
 echo "Restart Firefox after closing all tabs"
